2b179843bae7e7150f398ab8ab1f0b728950143f
AlbertMosella <mosella.albert@gmail.com>
2022-05-14 15:08:59 +0200

    Installed cors dependencies

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const robotsRouter = require("./routers/robotsRouters");
 const { errorDefault, notFoundError } = require("./middlewares/errors");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 app.use("/", robotsRouter);
 app.use(notFoundError);
