bbf58b2bd73c16f259a612bd83f2641c89851e12
AlbertMosella <mosella.albert@gmail.com>
2022-05-17 19:32:13 +0200

    Added auth, user controller and router

--- server/index.js
+++ server/index.js
@@ -2,15 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
 const robotsRouter = require("./routers/robotsRouters");
+const usersRouter = require("./routers/usersRouters");
 const { errorDefault, notFoundError } = require("./middlewares/errors");
 
 const app = express();
 
-app.use(morgan("dev"));
 app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use("/", robotsRouter);
+app.use("/users", usersRouter);
 app.use(notFoundError);
 app.use(errorDefault);
 
